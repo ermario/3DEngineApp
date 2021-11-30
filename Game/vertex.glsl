f95333582b0e3216af9ebf320a7765bdf60e7d4d
ermario <marius.dambean@gmail.com>
2021-11-30 21:25:50 +0100

    +Fixed square in sceen + added DevIL +started to code for loading texture.

--- Game/vertex.glsl
+++ Game/vertex.glsl
@@ -1,12 +1,15 @@
 # version 330
 layout(location=0) in vec3 my_vertex_position;
+layout(location = 1) in vec2 vertex_uv0;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
 
+out vec2 uv0;
 
 void main()
 {
     gl_Position = proj * view *  model * vec4(my_vertex_position, 1.0);
+	uv0 = vertex_uv0;
 }
--- Soruce/ModuleRenderExercise.cpp
+++ Soruce/ModuleRenderExercise.cpp
@@ -5,14 +5,17 @@
 #include "ModuleProgram.h"
 #include "ModuleCamera.h"
 
+
 float cube_vertices[] = {
     -1.0f, 1.0f, 0.0f, // triángulo 1 : comienza
-     1.0f,-1.0f, 0.0f,
      1.0f,-1.0f, 0.0f, 
+    -1.0f,-1.0f, 0.0f,
     -1.0f, 1.0f, 0.0f, // triángulo 2 : comienza
-     1.0f,-1.0f, 0.0f,
-    -1.0f,-1.0f, 0.0f 
+     1.0f, 1.0f, 0.0f,
+     1.0f,-1.0f, 0.0f
 };
+
+
 static const GLfloat g_vertex_buffer_data[] = {
     -1.0f,-1.0f,-1.0f, // triángulo 1 : comienza
     -1.0f,-1.0f, 1.0f,
@@ -59,6 +62,16 @@ ModuleRenderExercise::ModuleRenderExercise()
 bool ModuleRenderExercise::Init()
 {
 
+    glGenBuffers(1, &vertex_buffer_object);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+    // size = 3 float per vertex
+    // stride = 0 is equivalent to stride = sizeof(float)*3
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glEnableVertexAttribArray(0);
+
+
 
 	return true;
 }
@@ -73,31 +86,20 @@ bool ModuleRenderExercise::CleanUp()
 
 update_status ModuleRenderExercise::Update()
 {
+    float4x4 model(float4(5.0f, 0.0f, 0.0f, 0.0f), float4(0.0f, 5.0f, 0.0f, 0.0f), float4(0.0f, 0.0f, 5.0f, 0.0f), float4(0.0f, 0.0f, 0.0f, 1.0f));
+    unsigned int program_id = App->shader_program->GetProgramId();
+    glUseProgram(program_id);
+    //MODEL
+    //model = float4x4::identity;
 
-	glGenBuffers(1, &vertex_buffer_object);
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
-	// size = 3 float per vertex
-	// stride = 0 is equivalent to stride = sizeof(float)*3
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glEnableVertexAttribArray(0);
-	unsigned int program_id = App->shader_program->GetProgramId();
-	glUseProgram(program_id);
-
-	float4x4 model, view, projection;
-
-	//MODEL
-	//model = float4x4::FromTRS(float3(2.0f, 0.0f, 0.0f),float4x4::RotateZ(pi / 4.0f),float3(2.0f, 1.0f, 0.0f));
-	model = float4x4::identity;
 	glUniformMatrix4fv(glGetUniformLocation(program_id, "model"), 1, GL_TRUE, &model[0][0]);
 	glUniformMatrix4fv(glGetUniformLocation(program_id, "view"), 1, GL_TRUE, &App->camera->GetViewMatrix()[0][0]);
 	glUniformMatrix4fv(glGetUniformLocation(program_id, "proj"), 1, GL_TRUE, &App->camera->GetProjectionMatrix()[0][0]);
 
 	// TODO: bind buffer and vertex attributes
-	glBindVertexArray(vertex_buffer_object);
+	//glBindVertexArray(vertex_buffer_object);
 	// 1 triangle to draw = 3 vertices 
-	glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 	
     return UPDATE_CONTINUE;
 }
